2a8d55622809c7f49be1943fc6bd7bab42a0c65f
jferragut <31458614+jferragut@users.noreply.github.com>
2019-11-11 14:06:43 -0500

    test get method catch

--- api/student/index.py
+++ api/student/index.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>', methods=["POST"])
+@app.route('/<path:path>', methods=["GET","POST"])
 def catch_all(path):
     if request.method=="POST":
         try:
@@ -15,10 +15,12 @@ def catch_all(path):
             if student == None:
                 return jsonify("Student name not provided. Please check your request and try again."),400
             else:
-                return jsonify({"Success. Student:  {}"}.format(student)),200
+                return jsonify({"Status":"Success. Received  {}"}.format(student)),200
 
         except Exception as e:
             return "An exception of type {0} occurred. \nArguments: {1!r}".format(
                 type(e).__name__, e.args)
+    elif request.method=="GET":
+        return jsonify({"Status":"Get method {}"}.format(request.json))
     else:
         return jsonify("You have used an unsupported request type. Please check the type and try again."),404
